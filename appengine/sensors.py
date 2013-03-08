cbe410da4f1fe317c0790cbded43937276982ece
sgzmd <sigizmund@gmail.com>
2013-03-08 17:38:02 +0000

    Working app with a single method.

--- appengine/sensors.py
+++ appengine/sensors.py
@@ -3,12 +3,14 @@ import random
 import re
 
 from google.appengine.ext import endpoints
-from protorpc import remote, messages
-
 from google.appengine.ext import db
 
+from protorpc import remote, messages
+
+# Replace this with your JS/Android/iOS client ID
 CLIENT_ID = 'YOUR-CLIENT-ID'
 
+# Represents a single sensor on API level
 class SensorMessage(messages.Message):
   class Room(messages.Enum):
     LIVING_ROOM = 1
@@ -19,15 +21,20 @@ class SensorMessage(messages.Message):
     TEMPERATURE = 1
     MOTION = 2
 
+  # a unique identifier of sensor in this system
   sensor_id = messages.IntegerField(1, required=True)
+
+  # e.g. sensor IP address
   network_id = messages.StringField(2)
   room = messages.EnumField('SensorMessage.Room', 3, default='LIVING_ROOM')
   sensor_type = messages.EnumField('SensorMessage.Type', 4, default='TEMPERATURE')
   active = messages.BooleanField(5, default=True)
 
+# Contains potentially multiple sensors
 class SensorsRequestMessage(messages.Message):
   sensors = messages.MessageField('SensorMessage', 1, repeated=True)
 
+# Sent in response to Add / Update operations
 class SensorsResponseMessage(messages.Message):
   class Status(messages.Enum):
     STATUS_OK = 1
@@ -36,6 +43,7 @@ class SensorsResponseMessage(messages.Message):
   status = messages.EnumField('SensorsResponseMessage.Status', 1, default='STATUS_OK')
   error = messages.StringField(2, repeated = True)
 
+# Represents a single sensor in application's database
 class Sensor(db.Model):
   sensor_id = db.IntegerProperty(required = True)
   network_id = db.StringProperty()
@@ -67,11 +75,15 @@ class SensorsApi(remote.Service):
       for s in request.sensors:
         sensorEntity = Sensor(sensor_id=s.sensor_id,
                               network_id=s.network_id,
-                              room=s.room,
-                              sensor_type=s.sensor_type,
+                              room=str(s.room),
+                              sensor_type=str(s.sensor_type),
                               active=s.active)
         sensorEntity.put()
 
       response = SensorsResponseMessage()
-      response.status = 'STATUS_OK'
+      response.status = SensorsResponseMessage.Status.STATUS_OK
 
+      return response
+
+APPLICATION = endpoints.api_server([SensorsApi],
+                                   restricted=False)
