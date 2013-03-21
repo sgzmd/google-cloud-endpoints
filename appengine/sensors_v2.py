cdb71d50e2ae4e59c49cec3e8ecb5115de5678db
sgzmd <sigizmund@gmail.com>
2013-03-21 18:28:34 +0000

    saving changes before moving to other branch

--- appengine/sensors_v2.py
+++ appengine/sensors_v2.py
@@ -4,12 +4,21 @@ from protorpc import remote
 
 from endpoints_proto_datastore.ndb import EndpointsModel
 
-
 # Represents a single sensor in application's database,
 # inheriting it from EndpointsModel.
 class Sensor(EndpointsModel):
-  sensor_id = db.IntegerProperty(required = True)
+  # indicates which members of this class are in fact attributes
+  # id is provided automagically
+  _message_fields_schema = ('id',
+                            'network_id',
+                            'room',
+                            'sensor_type',
+                            'active',
+                            'created')
+
   network_id = db.StringProperty()
   room = db.StringProperty()
   sensor_type = db.StringProperty()
   active = db.BooleanProperty()
+  created = ndb.DateTimeProperty(auto_now_add=True)
+
